db376735a5573455e3109ed601d6ed2256866be5
Tachikoma <section9lab@gmail.com>
2022-04-06 18:26:07 +0800

    update

--- docs/.vuepress/configs/navbar_config.ts
+++ docs/.vuepress/configs/navbar_config.ts
@@ -3,7 +3,7 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 export const zh: NavbarConfig = [
   {
     text: '关于博客',
-    link: '/',
+    link: '/guide/',
   },
   {
     text: 'oscp 101',
@@ -15,28 +15,6 @@ export const zh: NavbarConfig = [
           '/zh/reference/config.md',
           '/zh/reference/frontmatter.md',
           '/zh/reference/components.md',
-          '/zh/reference/plugin-api.md',
-          '/zh/reference/theme-api.md',
-          '/zh/reference/client-api.md',
-          '/zh/reference/node-api.md',
-        ],
-      },
-      {
-        text: '打包工具',
-        children: [
-          '/zh/reference/bundler/vite.md',
-          '/zh/reference/bundler/webpack.md',
-        ],
-      },
-      {
-        text: '默认主题',
-        children: [
-          '/zh/reference/default-theme/config.md',
-          '/zh/reference/default-theme/frontmatter.md',
-          '/zh/reference/default-theme/components.md',
-          '/zh/reference/default-theme/markdown.md',
-          '/zh/reference/default-theme/styles.md',
-          '/zh/reference/default-theme/extending.md',
         ],
       },
     ],
@@ -49,11 +27,6 @@ export const zh: NavbarConfig = [
         children: [
           '/zh/reference/plugin/back-to-top.md',
           '/zh/reference/plugin/container.md',
-          '/zh/reference/plugin/external-link-icon.md',
-          '/zh/reference/plugin/google-analytics.md',
-          '/zh/reference/plugin/medium-zoom.md',
-          '/zh/reference/plugin/nprogress.md',
-          '/zh/reference/plugin/register-components.md',
         ],
       },
       {
@@ -61,20 +34,6 @@ export const zh: NavbarConfig = [
         children: [
           '/zh/reference/plugin/docsearch.md',
           '/zh/reference/plugin/search.md',
-        ],
-      },
-      {
-        text: 'PWA',
-        children: [
-          '/zh/reference/plugin/pwa.md',
-          '/zh/reference/plugin/pwa-popup.md',
-        ],
-      },
-      {
-        text: '语法高亮',
-        children: [
-          '/zh/reference/plugin/prismjs.md',
-          '/zh/reference/plugin/shiki.md',
         ],
       },
     ],
--- docs/guide/README.md
+++ docs/guide/README.md
@@ -1,2 +1,13 @@
-# 测试001
+# 概要
 
+## 第一章
+> 这是一个测试Blog
+
+## 第二章
+
+```bash
+#!/bin/bash
+
+echo "hello world"
+
+```
